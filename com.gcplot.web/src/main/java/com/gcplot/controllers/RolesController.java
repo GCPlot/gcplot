645ce9ac5e51fa738a710dbc92b2adfc3d69e95c
Artem Dmitriev <art.dm.ser@gmail.com>
2016-08-31 08:46:23 +0300

    Roles Controller initial.

---
+++ com.gcplot.web/src/main/java/com/gcplot/controllers/RolesController.java
@@ -0,0 +1,13 @@
+package com.gcplot.controllers;
+
+/**
+ * @author <a href="mailto:art.dm.ser@gmail.com">Artem Dmitriev</a>
+ *         8/31/16
+ */
+public class RolesController extends Controller {
+
+    public void init() {
+
+    }
+
+}
