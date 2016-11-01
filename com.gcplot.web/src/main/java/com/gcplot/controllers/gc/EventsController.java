d635313fa5a5178cd2f556fa1e81c0e8ea2e97d0
Artem Dmitriev <art.dm.ser@gmail.com>
2016-11-01 07:56:07 +0300

    temp logging.

--- com.gcplot.web/src/main/java/com/gcplot/controllers/gc/EventsController.java
+++ com.gcplot.web/src/main/java/com/gcplot/controllers/gc/EventsController.java
@@ -215,6 +215,7 @@ public class EventsController extends Controller {
             Iterator<GCEvent> eventIterator = eventRepository.lazyPauseEvents(pp.getAnalyseId(), pp.getJvmId(),
                     Range.of(pp.getFrom(), pp.getTo()));
             while (eventIterator.hasNext()) {
+                LOG.info("Writing ...");
                 ctx.response(GCEventResponse.from(eventIterator.next(), pp.getTimeZone()));
             }
             ctx.finish();
