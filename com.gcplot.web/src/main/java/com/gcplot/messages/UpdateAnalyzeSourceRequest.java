8cc5f3e1d7b49526ec7a50ceb59af7590ae9d4ac
Artem Dmitriev <art.dm.ser@gmail.com>
2017-03-26 07:23:20 +0300

    fix.

--- com.gcplot.web/src/main/java/com/gcplot/messages/UpdateAnalyzeSourceRequest.java
+++ com.gcplot.web/src/main/java/com/gcplot/messages/UpdateAnalyzeSourceRequest.java
@@ -17,30 +17,15 @@ public class UpdateAnalyzeSourceRequest {
     @JsonProperty(value = "id", required = true)
     public String id;
     @JsonProperty(value = "source_type", required = true)
-    @JsonDeserialize(using = SourceTypeDeserializer.class)
     public SourceType sourceType;
     @JsonProperty(value = "source_config", required = true)
     public String sourceConfig;
 
     public UpdateAnalyzeSourceRequest(@JsonProperty(value = "id", required = true) String id,
-                                      @JsonProperty(value = "source_type", required = true)
-                                      @JsonDeserialize(using = SourceTypeDeserializer.class) SourceType sourceType,
+                                      @JsonProperty(value = "source_type", required = true) SourceType sourceType,
                                       @JsonProperty(value = "source_config", required = true) String sourceConfig) {
         this.id = id;
         this.sourceType = sourceType;
         this.sourceConfig = sourceConfig;
-    }
-
-    public static class SourceTypeDeserializer extends JsonDeserializer<SourceType> {
-
-        @Override
-        public Class<?> handledType() {
-            return SourceType.class;
-        }
-
-        @Override
-        public SourceType deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-            return SourceType.by(p.getValueAsString());
-        }
     }
 }
